d206c0286cbd8da45eff7cd6f493840aa70945c3
Joy Gogna <dasani407@gmail.com>
2021-02-13 13:49:37 -0500

    first commit

---
+++ array.php
@@ -0,0 +1,15 @@
+<?php
+
+$names = array("Illyana"=>"has a corvette" , "William" => "apart of a milionare family business" ,"Jake" =>"CEO of a big time company" , "Dasani" => "big time actress" ,"Kyle" => "Famous surfer");
+
+foreach($names as $x => $val)
+{
+    echo "$x = $val<br>";
+}
+"<br>";
+$ValenciaEast = array("name" => "bld 5", "info"=> " is the student services basically the main building to get registered and grab books", "name2" => "bld3", "info2" => "  is the arts and entertainment building for performances", "name3"=> "bld1"   , "info3"=>" is the science building where science happens", "name4"=>"bld4","info4"=>" is the library and learning support where you can get help on your studies and do research", "name5"=> "bld2","info5"=> " are classrooms for electives or for club meetings");
+"<br>";
+foreach ($ValenciaEast as $x => $val){
+    echo "$x = $val<br>";
+}
+?>
